04ec471677b252424b19e4f407adbbb894bb8e50
Naveena <vishnaveena@gmail.com>
2020-10-24 23:57:40 -0500

    initial set up

---
+++ InstaLike/InstaLike/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InstaLike
+//
+//  Created by Naveena vishnu on 10/24/20.
+//  Copyright © 2020 vishnaveena. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
