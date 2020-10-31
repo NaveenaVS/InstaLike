41a5914468e73f0e49659a03086cb06a743bebcd
Naveena <vishnaveena@gmail.com>
2020-10-30 22:01:57 -0500

    completed till adding a random fake comment

--- InstaLike/InstaLike/FeedViewController.swift
+++ InstaLike/InstaLike/FeedViewController.swift
@@ -34,7 +34,8 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
         //making queries: look at doc
         let query = PFQuery(className:"Posts")
         //need inclode key:
-        query.includeKey("author") //fethcing key cus we wanna fetch the onj
+        //changed for part2 cus we wanna bring in comments also
+        query.includeKeys(["author", "comments", "comments.author"]) //fethcing key cus we wanna fetch the onj
         query.limit = 20
         
         //actually performing the configured query:
@@ -49,33 +50,88 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
     
     //need to impl functions cus we added UITableViewDelegate, UITableViewDataSource
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return posts.count;
+        let post = posts[section]
+        let comments = (post["comments"] as? [PFObject]) ?? []
+        return comments.count + 1;
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+         return posts.count;
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell //cus wanna access outlets I created
+        let post = posts[indexPath.section]
+        let comments = (post["comments"] as? [PFObject]) ?? []
         
-        //grabbing post:
-        let post = posts[indexPath.row] //indexPath.row to grab particular row
         
-        let user = post["author"] as! PFUser
-        cell.usernameLabel.text = user.username
+         //what type of cell to return? a;ways gonna be the zeroth path
+        if indexPath.row == 0{
+            //if true then its def gonna be a post cell
+            let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell //cus     wanna access outlets I created
         
-        //setting caption label:
-        cell.captionLabel.text = post["caption"] as! String
+//            //grabbing post: moved to top during part 2
+//            let post = posts[indexPath.row] //indexPath.row to grab particular row
         
-        //need to grab the image url:
-        let imageFile = post["image"] as! PFFileObject
-        let urlString = imageFile.url!
-        let url = URL(string: urlString)!
+            let user = post["author"] as! PFUser
+            cell.usernameLabel.text = user.username
         
-        cell.photoView.af_setImage(withURL: url)
+            //setting caption label:
+            cell.captionLabel.text = post["caption"] as! String
         
-        return cell
+            //need to grab the image url:
+            let imageFile = post["image"] as! PFFileObject
+            let urlString = imageFile.url!
+            let url = URL(string: urlString)!
+        
+            cell.photoView.af_setImage(withURL: url)
+        
+            return cell
+        } else{
+            //want another cell type:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "abcd") as! CommentCell
+            
+            //configuring the comments:
+            let comment = comments[indexPath.row - 1] // cus =0; that will be the post; 0th comment is when indexpath.row = 1
+            cell.commentLabel.text =  comment["text"] as? String
+            
+            let user = comment["author"] as! PFUser
+            cell.nameLabel.text = user.username
+            
+            return cell
+        }
     }
     
-
+//creating comments: fake
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //get call back every time user taps on the img
+        //task: choose post and add comment:
+        //select row
+        let post = posts[indexPath.row]
+        //create comment object
+        let comment =  PFObject(className: "Comments")
+        comment["text"] = "Rondome comment for testing"
+        //want comment to know which post its realted to
+        comment["post"] = post
+        //who created the comment
+        comment["author"] = PFUser.current()!
+        
+        //relationship magic:
+        
+        post.add(comment, forKey: "comments") //i think every post should have an array of comments -> add this comment to the array
+        
+        //smart abt what it saves: Parse
+        post.saveInBackground { (success, error) in
+            if success {
+                print("comment saved")
+            }else{
+                print("Error saving comment")
+            }
+        }
+        
+    }
+    
+    
     /*
     // MARK: - Navigation
 
@@ -106,7 +162,6 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
         delegate.window?.rootViewController = loginViewController
         
     }
-    
     
     
 }
